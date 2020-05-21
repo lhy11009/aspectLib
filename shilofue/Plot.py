e023ffbaba180f21fae454a145fb2eb6fec45662
Haoyuan Li <hylli@ucdavis.com>
2020-05-20 18:32:01 -0700

    small changes

--- shilofue/Plot.py
+++ shilofue/Plot.py
@@ -5,49 +5,38 @@ import json
 import re
 from matplotlib import pyplot as plt
 from importlib import resources
-import shilofue.data
-
-jsonDir = 'json'  # json files folder
-
-
-def JsonDump(_dict, _name):
-    '''
-    JsonDump(_dict, _name):
-    Dump configure in json
-
-    '''
-    if not os.path.isdir(jsonDir):
-        os.mkdir(jsonDir)
-    filename = os.path.join(jsonDir, "%s.json" % _name)
-    with open(filename, 'w') as fout:
-        json.dump(_dict, fout)
-
-
-def JsonRead(_filename):
-    '''
-    JsonRead(_filename):
-    Read configuration from json
-
-    '''
-    with resources.open_text(shilofue.data, _filename) as fin:
-        data = json.load(fin)
-    return data
-
+import shilofue.json
 
 def PlotOptions(prefix):
     '''
-    PlotOptions(prefix):
-    Format options
-
+    PlotOptions(prefix) 
+    Read format options for plotting from json files
+    Args:
+        prefix(string):
+            a prefix for options, all json files with this prefix will be imported
+    Returns:
+        options(dict):
+            a dictionary for options. Every match with the prefix will generate a 
+            key in this dictionary, while the value is read from a json file.
+            For example, if the prefix is 'Statistics' and one json file is
+            'Statistics_Number_of_Cells.json', then there will be a key called
+            'Number_of_Cells' in this dictionary.
     '''
-    Options = {}
-    for dirname, dirnames, filenames in os.walk(jsonDir):
-        for filename in filenames:
-            if re.match("^" + prefix, filename):
-                _name = re.sub("^.*?_", '', filename)
-                _name = re.sub(r".json", '', _name)
-                Options[_name] = JsonRead(filename)
-    return Options
+    _options = {}  # options is a dictionary
+    for _filename in resources.contents(shilofue.json):
+        # this resources.contents return a interable from a sub-package,
+        # entries in this interable are filenames
+        if re.match("^" + prefix + '_', _filename):
+            # the following two lines eliminate the words before the first '_'
+            # with that '_' as well as the '.json' in the end.
+            # so that if filename is 'Statistics_Number_of_Cells.json',
+            # _name is 'Number_of_Cells'
+            _name = re.sub("^.*?_", '', _filename) # when 
+            _name = re.sub(r".json", '', _name)
+            with resources.open_text(shilofue.json, _filename) as fin:
+                _options[_name] = json.load(fin)  # values are entries in this file
+    assert(_options is not {})  # assert not vacant
+    return _options
 
 
 class UNITCONVERT():
@@ -65,9 +54,10 @@ class UNITCONVERT():
         '''
         pass
 
-    def __init__(self, filename):
-        data = JsonRead(filename)
-        self.Converts = data['Converts']
+    def __init__(self, _filename):
+        with resources.open_text(shilofue.json, _filename) as fin:
+            _data = json.load(fin)
+        self.Converts = _data['Converts']
 
     def __call__(self, _units):
         for option in self.Converts:
@@ -85,7 +75,28 @@ class STATISTICS():
     '''
 
     def __init__(self):
-        self.Options = PlotOptions('Statistics')
+        self.options = PlotOptions('Statistics')
+    
+    def __call__(self, _filename, **kwargs):
+        '''
+        Read and plot
+        '''
+        # canvas = kwargs.get('canvas', np.array([1, 1]))
+        # ptype = kwargs['ptype']
+        # Read plot options from a json file
+        _jsonfile = kwargs.get('json', None)
+        if _jsonfile is not None:
+            with open(_jsonfile, 'r') as fin:
+                _configs = json.load(fin)
+        else:
+            # default is to read from 'Statistics.json' in shilofue.json
+            with resources.open_text(shilofue.json, 'Statistics.json') as fin:
+                _configs = json.load(fin)
+        self.ReadSTSHead(_filename)
+        self.ReadSTS(_filename)
+        # fileout = self.PlotSTSCombine(names=ptype, canvas=canvas)
+        _fileout = self.PlotSTSCombine(_configs)
+        return _fileout
 
     def ReadSTSHead(self, filename):
         '''
@@ -127,69 +138,88 @@ class STATISTICS():
         # This file starts with lines of comment
         assert(os.access(filename, os.R_OK))
         self.data = np.genfromtxt(filename, comments='#')
+    
+    def PlotSTSCombine(self, _configs):
+        '''
+        Combine all plottings
+        Arguments:
+            _configs(dict):
+                canvas(ndarray):
+                    layout of canvas
+                types(list):
+                    types of ploting, should have the 
+                    same size with canvas
+        Returns:
+            _filename(string):
+                name of the plotting created
+        Raises:
+            AssertionError:
+                if size of canvas doesn't match number of plottings
+        '''
+        # plot configuration
+        _canvas = _configs.get('canvas', [1, 1])
+        assert(type(_canvas) is list and len(_canvas) == 2)
+        _types = _configs['types']  # types of plotting
+        assert(type(_types) is list and
+            _canvas[0] * _canvas[1] == len(_types))  # size of canvas match size of _types
+        # plot
+        fig, axs = plt.subplots(_canvas[0], _canvas[1])
+        for i in range(len(_types)):
+            _type = _types[i]
+            _opt = self.options[_type]  # get the plot options
+            try:
+                # fix the bug where there is only one plot
+                self.PlotSTS(axs[i], _opt)
+            except TypeError:
+                self.PlotSTS(axs, _opt)
+        fig.tight_layout()
+        _filename = './Statistics.pdf'
+        fig.savefig(_filename)
+        plt.close(fig)
+        return _filename
 
-    def PlotSTS(self, fig, ax, _opt):
+    def PlotSTS(self, _ax, _opt):
         '''
         PlotSTS(self, fig, ax, _opt):
-        Plot statistic from data
+        Plot a statistic from data
+        Arguments:
+            _ax(axis):
+                an axis object from matplotlib
+            _opt(dict):
+                dictionary for plot options
+        Returns:
+            _ax(axis):
+                the same as input
         '''
-        # xname and yname are from json file, then column is determined
-        # by header information ####
-        xname = _opt.get('xname', 'Time')
-        yname = _opt.get('yname', 'Number_of_mesh_cells')
-        color = _opt.get('color', 'r')
-        Label = _opt.get('label', 'default')
-        Line = _opt.get('line', '-')
-        print("xname:", xname)
-        print("yname:", yname)  # debug
-        colx = self.header[xname]['col']
-        coly = self.header[yname]['col']
-        unitx = self.header[xname]['unit']
-        unity = self.header[yname]['unit']
-        x = self.data[:, colx]
-        y = self.data[:, coly]
-        ax.plot(x, y, '%s%s' % (Line, color), label=Label)
-        # construct label from xname and yname ####
-        if unitx is not None:
-            xLabel = re.sub("_", " ", xname) + ' (' + unitx + ')'
+        # Options for plot
+        # xname and yname are labels for x and y axis,
+        # color is the color for ploting
+        # label is the label for legend
+        # line is the type of line for plotting
+        _xname = _opt.get('xname', 'Time')
+        _yname = _opt.get('yname', 'Number_of_mesh_cells')
+        _color = _opt.get('color', 'r')
+        _label = _opt.get('label', 'default')
+        _line = _opt.get('line', '-')
+        # then column is determined
+        # by header information
+        _colx = self.header[_xname]['col']
+        _coly = self.header[_yname]['col']
+        _unitx = self.header[_xname]['unit']
+        _unity = self.header[_yname]['unit']
+        _x = self.data[:, _colx]
+        _y = self.data[:, _coly]
+        _ax.plot(_x, _y, _line, color=_color, label=_label)
+        # construct label from xname and yname
+        # put in unit
+        if _unitx is not None:
+            _xlabel = re.sub("_", " ", _xname) + ' (' + _unitx + ')'
         else:
-            xLabel = re.sub("_", " ", xname)
-        if unity is not None:
-            yLabel = re.sub("_", " ", yname) + ' (' + unity + ')'
+            _xlabel = re.sub("_", " ", _xname)
+        if _unity is not None:
+            _ylabel = re.sub("_", " ", _yname) + ' (' + _unity + ')'
         else:
-            yLabel = re.sub("_", " ", yname)
-        ax.set(xlabel=xLabel, ylabel=yLabel)
-        ax.legend()
-        fig.tight_layout()
+            _ylabel = re.sub("_", " ", _yname)
+        _ax.set(xlabel=_xlabel, ylabel=_ylabel)
+        _ax.legend()
 
-    def PlotSTSCombine(self, **kwargs):
-        '''
-        Plot statistic from data
-        '''
-        canvas = kwargs.get('canvas', np.array([1, 1]))
-        ptype = kwargs['ptype']  # types()
-        print("ptype: ", ptype)  # debug
-        fig, axs = plt.subplots(canvas[0], canvas[1])
-        for i in range(len(ptype)):
-            _type = ptype[i]
-            _opt = self.Options[_type]
-            try:
-                self.PlotSTS(fig, axs[i], _opt)
-            except TypeError:
-                self.PlotSTS(fig, axs, _opt)
-        filename = './Statistics.pdf'
-        fig.savefig(filename)
-        plt.close(fig)
-        return filename
-
-    def __call__(self, filename, **kwargs):
-        '''
-        Read and plot
-        '''
-        canvas = kwargs.get('canvas', np.array([1, 1]))
-        ptype = kwargs['ptype']
-        self.ReadSTSHead(filename)
-        self.ReadSTS(filename)
-        print(self.header)
-        fileout = self.PlotSTSCombine(ptype=ptype, canvas=canvas)
-        return fileout
--- tests/integration/test_plot.py
+++ tests/integration/test_plot.py
@@ -10,17 +10,11 @@ def test_plot_statistics():
     if(os.path.isfile('Statistics.pdf')):
         # remove previous files
         os.remove('Statistics.pdf')
-    # test_file = 'fixtures/parse_test.prm'
+    # test_file = 'fixtures/statistics'
     test_file = os.path.join(os.path.dirname(__file__), 'fixtures', 'statistics')
     assert(os.access(test_file, os.R_OK))
-    # plot data ##########
-    config_file = 'Plot.json'
-    conf = Plot.JsonRead(config_file)
     # plot statistics ouput #####
-    statistics_conf = conf['statistics']
-    canvas = np.array(statistics_conf['canvas'])
-    ptype = statistics_conf['ptype']
     Statistics = Plot.STATISTICS()
-    Statistics(test_file, ptype=ptype, canvas=canvas)
+    Statistics(test_file)
     assert(os.path.isfile('Statistics.pdf'))  # assert that the file is generated successfully
     # os.remove('Statistics.pdf')  # remove this file after finished
