4e0563c8825090a185e857d7e9ec61e3bfcb75e7
Haoyuan Li <hylli@ucdavis.com>
2020-05-28 10:15:04 -0700

    add record.sh

--- test_ssh_command.sh
+++ test_ssh_command.sh
@@ -1,10 +1,58 @@
 # !/bin/bash
 
+source record.sh
+
 FILENAME="test.prm"  # filename for aspect
-ssh lochy@peloton.cse.ucdavis.edu << EOF
-    eval "cd \$TwoDSubduction_DIR/katrina_case/katrina_case_parse_inputs_1"
-    eval "pwd > ~/test_results"
-    if [[ -n "$FILENAME" && -e "$FILENAME" ]]; then
-        eval "submit_job.sh $FILENAME"
-    fi
+
+
+##
+# Print how to use
+
+usage()
+{
+    printf "\
+Submit a job to cluster with a slurm system
+
+Usage:
+  %s [options] [server_name] [file_name]
+
+Options:
+"
+}
+
+server_submit_job(){
+    ssh $1 << EOF
+        eval "cd $(dirname $2)"
+        take_record 'cd $(dirname $2)' '$HOME/server_runs'
+        eval "submit_job.sh $(basename $2)"
+        take_record 'submit_job.sh $(basename $2)' '$HOME/server_runs'
 EOF
+}
+
+main()
+{
+    while [ -n "$1" ]; do
+        param="$1"
+        case $param in
+            -h|--help)
+                usage
+                exit 0
+            ;;
+            #####################################
+            # Prefix path
+            -p)
+                shift # shift seems to move all parameters forward
+                PREFIX="${1}"
+            ;;
+            -p=*|--prefix=*)
+                PREFIX="${param#*=}" # '#' hear seems to eliminate '*=' from param, haoyuan
+                # replace '~' by $HOME
+                PREFIX=${PREFIX/#~\//$HOME\/}
+            ;;
+        esac
+        shift # go to next variable, so we can still do something
+    done
+    server_submit_job 'lochy@peloton.cse.ucdavis.edu' '\$TwoDSubduction_DIR/katrina_case/katrina_case_parse_inputs_1/test.prm'
+}
+
+main "$@"
