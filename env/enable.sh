404c8168c400530bac2a08cfe7433c605da65fec
Haoyuan Li <hylli@ucdavis.com>
2020-07-04 18:19:19 -0700

    small change

--- env/enable.sh
+++ env/enable.sh
@@ -1,5 +1,6 @@
 export ASPECT_LAB_DIR="$HOME/ASPECT_PROJECT/aspectLib"
 export TwoDSubduction_DIR="$HOME/ASPECT_PROJECT/TwoDSubduction"
+alias project_update="python -m shilofue.TwoDSubduction update -o /home/lochy/ASPECT_PROJECT/TwoDSubduction"
 alias plt_connect='ssh -X lochy@peloton.cse.ucdavis.edu'
 alias plt_update='process.sh update_from_server .output/job.log lochy@peloton.cse.ucdavis.edu'
 alias plt_download='process.sh update_outputs_from_server .output/job.log lochy@peloton.cse.ucdavis.edu'
