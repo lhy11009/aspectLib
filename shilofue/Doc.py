8b891ae54435daf47202999eb8f35524f84137c4
Haoyuan Li <hylli@ucdavis.com>
2020-07-05 15:49:45 -0700

    mark

--- shilofue/Doc.py
+++ shilofue/Doc.py
@@ -114,23 +114,23 @@ class MKDOC():
     '''
     A class to use with mkdocs
     Attributes:
-        case_name(str) - case name
+        name(str) - case name
         dir(str) - directory of case data
         imgs(list) - list of images
         new_files(dict) - name: new file
     '''
-    def __init__(self, _case_name, _dir):
+    def __init__(self, _name, _dir):
         '''
         Inputs:
-            _case_name(str) - case name
+            _name(str) - case name
             _dir(str) - directory of case data
         '''
-        self.case_name = _case_name
+        self.name = _name
         self.dir = _dir
         self.imgs = []
         self.new_files = {}
 
-    def append_img(self, _img):
+    def AttachImage(self, _img):
         '''
         Inputs:
             _img(list or str) - img files
@@ -154,59 +154,83 @@ class MKDOC():
         update = kwargs.get('update', False)
         append_prm = kwargs.get('append_prm', False)
         _prm = kwargs.get('prm', 'case.prm')
+        _type = kwargs.get('type', 'case')
         # These directory and files need to be preexist
         assert(os.path.isdir(_odir))
         assert(os.path.isdir(os.path.join(_odir, 'docs')))
         _mcdocs_file = os.path.join(_odir, 'mkdocs.yml')
         assert(os.path.isfile(_mcdocs_file))
         _index_file = os.path.join(_odir, 'docs', 'index.md')
-        _case_dir = os.path.join(_odir, 'docs', self.case_name)
-        if not os.path.isdir(_case_dir):
-            os.mkdir(_case_dir)
-        # Append a summary.md
-        if os.path.isfile(os.path.join(_case_dir, 'summary.md')):
-            if update == True:
-                self.GenerateCaseMkd(_case_dir, update=True)
+        if _type == 'case':
+            # a case, todo
+            self.AppendCase(_odir, update=update, append_prm=append_prm, prm=_prm)
+        elif _type == 'group':
+            # a group, todo
+            self.AppendGroup(_odir, update=update, append_prm=append_prm, prm=_prm)
+            pass
         else:
-            _filename = self.GenerateCaseMkd(_case_dir)
-            # in a mkdocs file, files are listed as 'case_name/_filename'
-            self.new_files['Summary'] = os.path.join(self.case_name, os.path.basename(_filename))
+            raise ValueError('type must be \'case\' or \'group\'')
+
+    def AppendCase(self, _odir, **kwargs):
+        '''
+        Append a case to doc
+        Inputs:
+            kwargs:
+                update(bool) - if update an existing case
+                append_prm(bool) - if append a prm file
+                prm(str) - name of a prm file
+        '''
+        update = kwargs.get('update', False)
+        append_prm = kwargs.get('append_prm', False)
+        _prm = kwargs.get('prm', 'case.prm')
+        _target_dir = os.path.join(_odir, 'docs', self.name)
+        if not os.path.isdir(_target_dir):
+            os.mkdir(_target_dir)
+        # Append a summary.md
+        if os.path.isfile(os.path.join(_target_dir, 'summary.md')):
+            if update == True:
+                self.GenerateCaseMkd(_target_dir, update=True)
+        else:
+            _filename = self.GenerateCaseMkd(_target_dir)
+            # in a mkdocs file, files are listed as 'name/_filename'
+            self.new_files['Summary'] = os.path.join(self.name, os.path.basename(_filename))
         # Append a prm file if the append_prm option is True
         if append_prm:
-            if os.path.isfile(os.path.join(_case_dir, _prm)):
+            if os.path.isfile(os.path.join(_target_dir, _prm)):
                 if update == True:
                     _prm_source_file = os.path.join(self.dir, _prm)
                     assert(os.path.isfile(_prm_source_file))
-                    copyfile(_prm_source_file, os.path.join(_case_dir, _prm))
+                    copyfile(_prm_source_file, os.path.join(_target_dir, _prm))
             else:
                 _prm_source_file = os.path.join(self.dir, _prm)
                 assert(os.path.isfile(_prm_source_file))
-                copyfile(_prm_source_file, os.path.join(_case_dir, _prm))
-                self.new_files['Parameters'] = os.path.join(self.case_name, _prm)
+                copyfile(_prm_source_file, os.path.join(_target_dir, _prm))
+                self.new_files['Parameters'] = os.path.join(self.name, _prm)
         if self.new_files != {}:
             self.RenewMkdocsYml(_odir)
 
-    def GenerateCaseMkd(self, _case_dir, **kwargs):
+    def GenerateCaseMkd(self, _target_dir, **kwargs):
         '''
         Generate markdown file of a case
         Inputs:
-            _case_dir(str): directory of this case
+            _target_dir(str): directory of this case
             kwargs:
                 filename(str): name of the file
         Returns:
             _filename(str): file generated
         '''
         _filename = kwargs.get('filename', 'summary.md')
-        _filename = os.path.join(_case_dir, _filename)
+        _filename = os.path.join(_target_dir, _filename)
         _auto_mkd_file = os.path.join(self.dir, 'auto.md')
         _extra_mkd_file = os.path.join(self.dir, 'extra.md')
         assert (os.path.isfile(_auto_mkd_file))
         copyfile(_auto_mkd_file, _filename)  # copy the auto.mkd file from case directory
         if (os.path.isfile(_extra_mkd_file)):
             # apppend contents of extra.mkd at the end of case.mkd
+            with open(_extra_mkd_file, 'r') as fin:
+                _contents = fin.read()
             with open(_filename, 'a') as fout:
-                with open(_extra_mkd_file, 'r') as fin:
-                    fout.write(fin.read())
+                fout.write(_contents)
         return _filename
 
     def RenewMkdocsYml(self, _odir):
@@ -236,13 +260,13 @@ class MKDOC():
         assert(_temp == _end - _start - 1)
         try:
             # this case is already in the nav part of the yml file
-            value = _nav_dict[self.case_name]
+            value = _nav_dict[self.name]
             my_assert(type(value) == dict, TypeError,
                       'entry for a case must be a single dict, prepared to include dictionary in the future')
             value = {**value, **self.new_files}  # merge and substitute value in first dict with value in second dict
         except KeyError:
             # this case is new to the yml file
-            _nav_dict[self.case_name] = self.new_files
+            _nav_dict[self.name] = self.new_files
         _new_lines = _lines[0: _start]
         _new_lines += ProduceNav(_nav_dict)
         _new_lines += _lines[_end: -1]
