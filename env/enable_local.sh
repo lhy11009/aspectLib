308c0c25da69ead44b53c6bd3562c7bd225f4c0b
Haoyuan Li <hylli@ucdavis.edu>
2021-05-27 15:58:55 -0700

    remove enable_local.sh from git log

--- env/enable_local.sh
+++
@@ -1,4 +0,0 @@
-export ASPECT_PROJECT_DIR="$HOME/ASPECT_PROJECT"
-export ASPECT_LAB_DIR="$HOME/Desktop/aspectLib"
-export TwoDSubduction_DIR="$HOME/ASPECT_PROJECT/TwoDSubduction"
-export TwoDSubduction_ucd_DIR="/home/lochy/ASPECT_PROJECT/TwoDSubduction"
