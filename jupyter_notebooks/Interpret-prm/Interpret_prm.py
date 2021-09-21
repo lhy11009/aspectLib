262f0a53f2f1d45136729fb40293c4185823784f
Haoyuan Li <hylli@ucdavis.edu>
2021-09-21 09:57:53 -0700

    Jupyter: add scripts to parse prm file from aspect

---
+++ jupyter_notebooks/Interpret-prm/Interpret_prm.py
@@ -0,0 +1,71 @@
+import re
+import os
+
+# In the following function, the inputs(prm file) is parsed to a dictionary
+# Each subsection in the prm file would be parsed to sub-dictionary relative to its upper level
+def ParseFromDealiiInput(fin):
+    """
+    ParseFromDealiiInput(fin)
+
+    Parse Dealii input file to a python dictionary
+    """
+    inputs = {}
+    line = fin.readline()
+    while line is not "":
+        # Inputs formats are
+        # comment: "# some comment"
+        # start and end of new section:
+        # "subsection name" and "end"
+        # set variables values:
+        # 'set key = val'
+        if re.match('^(\t| )*#', line):
+            # Skip comment lines, mark by '#' in file
+            pass
+        elif re.match('^(\t| )*set', line):
+            # Parse key and value
+            # from format in file as 'set key = val'
+            # to a dictionary inputs
+            # inputs[key] = val
+            temp = re.sub('^(\t| )*set ', '', line, count=1)
+            temp = temp.split('=', maxsplit=1)
+            key = temp[0]
+            key = re.sub('(\t| )*$', '', key)
+            # key = key.strip(' ')
+            value = temp[1]
+            value = re.sub('^ *', '', value)
+            value = re.sub(' *(#.*)?\n$', '', value)
+            while value[-1] == '\\':
+                # Deal with entries that extent to
+                # multiple lines
+                line = fin.readline()
+                line = re.sub(' *(#.*)?\n$', '', line)
+                value = value + '\n' + line
+            inputs[key] = value
+        elif re.match('^.*subsection', line):
+            # Start a new subsection
+            # Initialize new dictionary and interatively call function,
+            key = re.sub('^.*subsection ', '', line)
+            key = key.strip('\n')
+            try:
+                # Fix the bug where a subsection emerges
+                # multiple times
+                inputs[key]
+            except KeyError:
+                inputs[key] = ParseFromDealiiInput(fin)
+            else:
+                temp = ParseFromDealiiInput(fin)
+                inputs[key].update(temp.items())
+        elif re.match('^.*end', line):
+            # Terminate and return, marked by 'end' in file
+            return inputs
+        line = fin.readline()
+    return inputs
+
+# Compare the contents of 'inputs' below with the prm file to see what it is.
+# the 'Dimension' variable locates at the top level and it is a single entry in the dictionary.
+# 'Solver parameters' is a name for a subsection and it is a sub-dictionary itself.
+prm_path = 'case.prm'
+assert(os.access(prm_path, os.R_OK))
+with open(prm_path, 'r') as fin:
+    inputs = ParseFromDealiiInput(fin)
+print('Contents of inputs:\n', 'Dimension:', inputs['Dimension'], '\n', 'Solver parameters:\n', inputs['Solver parameters'])
