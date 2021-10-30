1e4c911ddaae51b36b7de7036661ec0821f5c799
Haoyuan Li <hylli@ucdavis.edu>
2021-10-29 22:38:17 -0700

    fix env

--- env/enable_peloton.sh
+++ env/enable_peloton.sh
@@ -1,10 +1,8 @@
 export ASPECT_PROJECT_DIR="$HOME/ASPECT_PROJECT"
 export ASPECT_SOURCE_DIR="/group/billengrp/lochy/softwares/aspect"
 export ASPECT_LAB_DIR="$HOME/ASPECT_PROJECT/aspectLib"
-export TwoDSubduction_DIR="$HOME/ASPECT_PROJECT/TwoDSubduction"
+export TwoDSubduction_DIR="/group/billengrp-mpi-io/lochy/TwoDSubduction"
 export SQUEUE="/share/apps/slurm-19.05.3/18.04/bin/squeue"
 export SCANCEL="/share/apps/slurm-19.05.3/18.04/bin/scancel"
 alias submit_case="slurm.sh -l /home/lochy/ASPECT_PROJECT/aspectLib/.output/job.log -lt ./output/machine_time"
 export LearnAspect_DIR="/home/lochy/ASPECT_PROJECT/LearnAspect"
-# world builder
-export WORLD_BUILDER_SOURCE_DIR="/group/billengrp/lochy/softwares/world_builder"
