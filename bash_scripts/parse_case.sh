0858a8c9349a1160e39a0a8149ccb12339c47f03
Haoyuan Li <hylli@ucdavis.edu>
2021-10-30 13:45:28 -0700

    parse_case.sh: show case run time info

--- bash_scripts/parse_case.sh
+++ bash_scripts/parse_case.sh
@@ -15,7 +15,33 @@
 ################################################################################
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd  )"
-source "${ASPECT_LAB_DIR}/bash_scripts/utilities.sh"
+source "${ASPECT_LAB_DIR}/utilities/bash_scripts/utilities.sh"
+
+
+usage(){
+  # usage of this script
+    _text="
+${BASH_SOURCE[0]}
+
+Parse job run time information
+
+Dependencies:
+   env:
+       ASPECT_LAB_DIR
+       ASPECT_SOURCE_DIR
+
+Example Usage:
+   case run time info (last step):
+       
+       Lib_parse_case.sh case_info /group/billengrp-mpi-io/lochy/TwoDSubduction/wb/wb_cart_4
+   
+   case run time info by given an id (must be running):
+
+      Lib_parse_case.sh case_info_with_id 250256 
+"
+    printf "${_text}"
+
+}
 
 
 parse_block_output(){
@@ -101,10 +127,53 @@ parse_block_output_to_file(){
     printf "${contents}" >> "${ofile}"
 }
 
+parse_run_time_info_last_step(){
+    # parse run time output
+    # $1(str): log file
+    # todo
+    [[ -e ${1} ]] || cecho ${BAD} "${FUNCNAME[0]}: logfile(${1}) doesn't exist"
+    local outputs=$(awk -f "${ASPECT_LAB_DIR}/bash_scripts/awk_states/parse_block_output" "$1")
+    IFS=$'\n'; local entries=(${outputs})
+    IFS=' '; return_value=(${entries[*]: -1})
+    printf "Step\t Time(unit defined in prm)\t Wall_clock(s)\n"
+    printf "${return_value[*]}\n"
+}
+
+parse_run_time_info_last_step_with_id(){
+    # parse run time output with job id
+    # $1(str): job id
+    # todo
+    locate_workdir_with_id "$1"
+    [[ $? == 1 ]] && return 1
+    [[ -d "${return_value}" ]] ||  { cecho ${BAD} "${FUNCNAME[0]}: work directory(${return_value}) doesn't exist"; exit 1; }
+    local log_file="${return_value}/output/log.txt"
+    [[ -e "${log_file}" ]] || { cecho "${BAD}" "${FUNCNAME[0]}: log file (${log_file}) doesn't exist"; exit 1; }
+    parse_run_time_info_last_step "${log_file}"
+    return 0
+}
+ 
+
+locate_workdir_with_id(){
+    # todo
+    # locate work directory of a job with job id
+    # $1 (str): job
+    local outputs=$(scontrol show job "$1" | grep WorkDir)
+    if [[ -n ${outputs} ]]; then
+        IFS='='; local entries=(${outputs})
+	return_value=${entries[*]: -1}
+	printf "workdir: ${return_value}\n"
+	return 0
+    else
+        cecho "${BAD}" "no job (id: $1) found"; return 1
+    fi
+}
+
 # future remove unneeded functions and add one to parse case info(i.e. id job given, case name, wall clock, core*hrs, step
 
 main(){
-    if [[ "$1" = "parse_block_results" ]]; then
+    if [[ "$1" = "-h" ]]; then
+        usage
+    elif [[ "$1" = "parse_block_results" ]]; then
         # this doesn't work for $3 with whitespace in it.
 	    [[ -n "$2" ]] || { cecho "${BAD}" "no log file given (\$2)"; exit 1; }
 	    [[ -n "$3" ]] || { cecho "${BAD}" "no key given (\$3)"; exit 1; }
@@ -116,6 +185,16 @@ main(){
         local log_file="$2"
         local ofile="$3"
         parse_block_output_to_file "${log_file}" "${ofile}" "Assemble Stokes system" "Solve Stokes system"
+    elif [[ "$1" = "case_info" ]]; then
+        # todo
+	[[ -n "$2" ]] || { cecho "${BAD}" "no case directory given (\$2)"; exit 1; }
+	[[ -d "$2" ]] || { cecho "${BAD}" "directory given (\$2) doesn't exist"; exit 1; }
+	log_file="$2/output/log.txt"
+	[[ -e "${log_file}" ]] || { cecho "${BAD}" "log file (${log_file}) doesn't exist"; exit 1; }
+	parse_run_time_info_last_step "${log_file}"
+    elif [[ "$1" = "case_info_with_id" ]]; then
+	[[ -n "$2" ]] || { cecho "${BAD}" "no id number given (\$2)"; exit 1; }
+	parse_run_time_info_last_step_with_id "$2"
     else
     	cecho "${BAD}" "option ${1} is not valid\n"
     fi
