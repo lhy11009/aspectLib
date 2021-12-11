dad3b45f74eea7b05587650c510ec23741401019
Haoyuan Li <hylli@ucdavis.edu>
2021-12-10 22:13:29 -0800

    add PhaseTransition.py

---
+++ shilofue/PhaseTransition.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+r"""Work with phase transitions in aspect
+
+This exports: 
+
+  -
+
+This depends on:
+
+  -  
+
+""" 
+import numpy as np
+import sys, os, argparse
+import json
+# import pathlib
+# import subprocess
+import numpy as np
+# from matplotlib import cm
+from matplotlib import pyplot as plt
+
+# directory to the aspect Lab
+ASPECT_LAB_DIR = os.environ['ASPECT_LAB_DIR']
+RESULT_DIR = os.path.join(ASPECT_LAB_DIR, 'results')
+# directory to shilofue
+shilofue_DIR = os.path.join(ASPECT_LAB_DIR, 'shilofue')
+# import utilities in subdirectiory
+sys.path.append(os.path.join(ASPECT_LAB_DIR, 'utilities', "python_scripts"))
+import Utilities
+
+
+def ParsePhaseInput(inputs):
+    '''
+    parse input of phases to a aspect input form
+    
+    '''
+    output = ""
+
+    # read in density of the base phase
+    rho_base = inputs.get("rho_base", 3300.0)
+    Utilities.my_assert(type(rho_base) == float, TypeError, "base value for density must be a float")
+    # read in the density change with each transtion
+    drho = inputs.get("drho", [0.0])
+    Utilities.my_assert(type(drho) == list, TypeError, "value of density change must be a list")
+    # read in the fraction with each transtion
+    xc = inputs.get("xc", [1.0])
+    Utilities.my_assert(type(xc) == list, TypeError, "value of fraction with transition must be a list")
+    # number of transition
+    total = len(drho)
+    Utilities.my_assert(len(xc) == total, ValueError, "length of xc and drho must be the same")
+
+    # compute density
+    rho = rho_base * np.ones(total + 1)
+    for i in range(total):
+        rho[i+1:] += drho[i] * xc[i]
+
+    # generate output
+    output += "%.1f" % rho[0]
+    for i in range(1, total+1):
+        output += "|%.1f" % rho[i]
+
+    return output
+
+def Usage():
+    print("\
+(One liner description\n\
+\n\
+Examples of usage: \n\
+\n\
+    - parse composition dependent phase transition inputs:\n\
+\n\
+        python -m shilofue.Parse phase_input -i ./files/TwoDSubduction/phases_1_0.json\n\
+\n\
+        ")
+
+def SomeFunction(foo):
+    '''
+    descriptions
+    Inputs:
+        -
+    Returns:
+        -
+    '''
+    pass
+
+
+def main():
+    '''
+    main function of this module
+    Inputs:
+        sys.arg[1](str):
+            commend
+        sys.arg[2, :](str):
+            options
+    '''
+    _commend = sys.argv[1]
+    # parse options
+    parser = argparse.ArgumentParser(description='Parse parameters')
+    parser.add_argument('-i', '--inputs', type=str,
+                        default='',
+                        help='Some inputs')
+    _options = []
+    try:
+        _options = sys.argv[2: ]
+    except IndexError:
+        pass
+    arg = parser.parse_args(_options)
+
+    # commands
+    if (_commend in ['-h', '--help']):
+        # example:
+        Usage()
+    elif _commend == 'phase_input':
+        # example:
+        Utilities.my_assert(os.access(arg.inputs, os.R_OK), FileExistsError, "Json file doesn't exist.")
+        with open(arg.inputs) as fin:
+            inputs = json.load(fin)
+
+        # get the outputs
+        outputs = "density = "
+        for key, value in inputs.items():
+            if type(value) == dict:
+                output = ParsePhaseInput(value)
+                outputs += "%s: %s, " % (key, output)
+
+        # print the output
+        print(outputs)
+    else:
+        # no such option, give an error message
+        raise ValueError('No commend called %s, please run -h for help messages' % _commend)
+
+# run script
+if __name__ == '__main__':
+    main()
---
+++ tests/unit/test_phase_transition.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+r"""Test for PhaseTransition.py
+
+This outputs:
+
+  - test results to value of variable "test_dir"
+
+This depends on:
+
+  - source files written from the value of "source_dir"
+
+Examples of usage:
+
+  - default usage:
+
+        python -m pytest test_foo.py
+
+descriptions:
+    every function is a separate test, combined usage with pytest module
+""" 
+
+
+import os
+# import pytest
+# import filecmp  # for compare file contents
+# import numpy as np
+# import shilofue.Foo as Foo  # import test module
+from shilofue.PhaseTransition import *
+# from matplotlib import pyplot as plt
+# from shutil import rmtree  # for remove directories
+
+test_dir = ".test"
+
+if not os.path.isdir(test_dir):
+    # check we have the directory to store test result
+    os.mkdir(test_dir)
+
+def test_parse_phase_input():
+    '''
+    Test parse phase input
+    '''
+    Inputs = {
+        "rho_base" : 3300.0,\
+        "drho" : [520.0, 250.0, 670.0, 840.0, 560.0, 597.0, 1170.0],\
+        "xc" : [0.55, 0.55, 0.05, 0.55, 0.4, 0.4, 0.4]
+    }
+    output = ParsePhaseInput(Inputs)
+    assert(output == "3300.0|3586.0|3723.5|3757.0|4219.0|4443.0|4681.8|5149.8")
+
+    
+# notes
+    
+# to check for error message
+    # with pytest.raises(SomeError) as _excinfo:
+    #    foo()
+    # assert(r'foo' in str(_excinfo.value))
+
+# assert the contents of file
+    # assert(filecmp.cmp(out_path, std_path))
+
