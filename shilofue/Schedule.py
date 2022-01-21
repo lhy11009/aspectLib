889023ea763f6bab27590a6c4aca130ad2723086
Haoyuan Li <hylli@ucdavis.edu>
2022-01-21 09:48:17 -0800

    add Schedule.py

---
+++ shilofue/Schedule.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+r"""Schedule tasks
+
+This exports: 
+
+  -
+
+This depends on:
+
+  -  
+
+Examples of usage:
+
+  - default usage:
+
+        python -m 
+
+descriptions
+""" 
+import sys, os, argparse
+# import json, re
+# import pathlib
+# import subprocess
+import numpy as np
+# from matplotlib import cm
+from matplotlib import pyplot as plt
+
+# directory to the aspect Lab
+ASPECT_LAB_DIR = os.environ['ASPECT_LAB_DIR']
+RESULT_DIR = os.path.join(ASPECT_LAB_DIR, 'results')
+# directory to shilofue
+shilofue_DIR = os.path.join(ASPECT_LAB_DIR, 'shilofue')
+# import utilities in subdirectiory
+sys.path.append(os.path.join(ASPECT_LAB_DIR, 'utilities', "python_scripts"))
+import Utilities
+
+def Usage():
+    print("\
+(One liner description\n\
+\n\
+Examples of usage: \n\
+\n\
+  - default usage: \n\
+\n\
+        python -m \
+        ")
+
+
+class SCHEDULE():
+    '''
+    Class for scheduling task
+    '''
+    def __init__(self):
+        '''
+        Initiation
+        '''
+        self.local_tasks = []
+        self.remote_tasks = []
+        pass
+
+    def add_task(self, **kwargs):
+        '''
+        add a task
+        kwargs:
+            remote(True or False): whether this is a remote command
+        '''
+        pass
+
+    def __call__(self):
+        '''
+        Prepare bash scripts and schedule
+        '''
+        pass
+
+
+
+
+def SomeFunction(foo):
+    '''
+    descriptions
+    Inputs:
+        -
+    Returns:
+        -
+    '''
+    pass
+
+
+def main():
+    '''
+    main function of this module
+    Inputs:
+        sys.arg[1](str):
+            commend
+        sys.arg[2, :](str):
+            options
+    '''
+    _commend = sys.argv[1]
+    # parse options
+    parser = argparse.ArgumentParser(description='Parse parameters')
+    parser.add_argument('-i', '--inputs', type=str,
+                        default='',
+                        help='Some inputs')
+    _options = []
+    try:
+        _options = sys.argv[2: ]
+    except IndexError:
+        pass
+    arg = parser.parse_args(_options)
+
+    # commands
+    if (_commend in ['-h', '--help']):
+        # example:
+        Usage()
+    elif _commend == 'foo':
+        # example:
+        SomeFunction('foo')
+    else:
+        # no such option, give an error message
+        raise ValueError('No commend called %s, please run -h for help messages' % _commend)
+
+# run script
+if __name__ == '__main__':
+    main()
