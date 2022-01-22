2f2fe3d1b05e7deb94e0fd1fa737eae3036fa8be
Haoyuan Li <hylli@ucdavis.edu>
2022-01-22 09:37:54 -0800

    follow

--- files/Project/PlotCase.py
+++ files/Project/PlotCase.py
@@ -13,9 +13,10 @@ Examples of usage:
 
   - default usage: plot case running results
 
-        Lib_TwoDSubduction0_PlotCase plot_case -i /home/lochy/ASPECT_PROJECT/TwoDSubduction/non_linear32/eba1_MRf12_iter20
+        Lib_FOO_PlotCase plot_case -i /home/lochy/ASPECT_PROJECT/TwoDSubduction/non_linear32/eba1_MRf12_iter20
 
 descriptions
+    First replate FOO with the name of the project
 """
 import numpy as np
 import sys, os, argparse, re
@@ -28,8 +29,7 @@ from matplotlib import pyplot as plt
 from shilofue.ParsePrm import ReadPrmFile
 from shilofue.PlotVisit import RunScripts
 import shilofue.PlotCombine as PlotCombine
-from shilofue.TwoDSubduction0.PlotVisit import VISIT_OPTIONS, PREPARE_RESULT_OPTIONS
-from shilofue.TwoDSubduction0.PlotSlab import vtk_and_slab_morph_case, SLABPLOT
+from shilofue.FOO.PlotVisit import VISIT_OPTIONS, PREPARE_RESULT_OPTIONS
 import shilofue.PlotCase as PlotCase
 import shilofue.PlotRunTime as PlotRunTime
 import shilofue.PlotStatistics as PlotStatistics
@@ -52,34 +52,24 @@ Examples of usage: \n\
 \n\
   - default usage: plot case running results, -t option deals with a time range, default is a whole range\n\
 \n\
-        Lib_TwoDSubduction0_PlotCase plot_case -i  ~/ASPECT_PROJECT/TwoDSubduction/EBA_CDPT1/eba_cdpt_SA80.0_OA40.0\
+        Lib_FOO_PlotCase plot_case -i  ~/ASPECT_PROJECT/TwoDSubduction/EBA_CDPT1/eba_cdpt_SA80.0_OA40.0\
  -t 0.0 -t1 0.5e6\n\
 \n\
   - plot cases in a directory (loop), same options as before:\n\
-        Lib_TwoDSubduction0_PlotCase  plot_case_in_dir -i ~/ASPECT_PROJECT/TwoDSubduction/EBA_CDPT\n\
+        Lib_FOO_PlotCase  plot_case_in_dir -i ~/ASPECT_PROJECT/TwoDSubduction/EBA_CDPT\n\
 \n\
   - prepare result of a single step by combining figures:\n\
-        Lib_TwoDSubduction0_PlotCase prepare_result_step -i ~/ASPECT_PROJECT/TwoDSubduction/test_peierls1/peierls\n\
+        Lib_FOO_PlotCase prepare_result_step -i ~/ASPECT_PROJECT/TwoDSubduction/test_peierls1/peierls\n\
 \n\
   - plot and then prepare result of a single step by combining figures:\n\
-        Lib_TwoDSubduction0_PlotCase plot_prepare_result_step -i ~/ASPECT_PROJECT/TwoDSubduction/test_peierls1/peierls\n\
+        Lib_FOO_PlotCase plot_prepare_result_step -i ~/ASPECT_PROJECT/TwoDSubduction/test_peierls1/peierls\n\
 \n\
   - generate animation: \n\
-        Lib_TwoDSubduction0_PlotCase animate_case -i ~/ASPECT_PROJECT/TwoDSubduction/test_peierls1/peierls\n\
+        Lib_FOO_PlotCase animate_case -i ~/ASPECT_PROJECT/TwoDSubduction/test_peierls1/peierls\n\
 \n\
   - generate animation for cases in a directory: \n\
-        Lib_TwoDSubduction0_PlotCase animate_case_in_dir -i `pwd`\n\
+        Lib_FOO_PlotCase animate_case_in_dir -i `pwd`\n\
 \n\
-  - generate slab_morph.txt: \n\
-    (what this does is looping throw all visualizing steps, so it takes time)\n\
-        Lib_TwoDSubduction0_PlotCase morph_case -i /home/lochy/ASPECT_PROJECT/TwoDSubduction/non_linear34/eba_low_tol_newton_shift_CFL0.8 \n\
-\n\
-  - plot trench movement: \n\
-    (note you have to have a slab_morph.txt generated) \n\
-        Lib_TwoDSubduction0_PlotCase plot_morph -i /home/lochy/ASPECT_PROJECT/TwoDSubduction/non_linear34/eba_low_tol_newton_shift_CFL0.8 \n\
-\n\
-  - generate slab_morph.txt for cases in a directory: \n\
-    Lib_TwoDSubduction0_PlotCase morph_case_in_dir -i ~/ASPECT_PROJECT/TwoDSubduction/EBA_CDPT1 \n\
         ")
 
 
@@ -94,30 +84,30 @@ def PlotCaseRun(case_path, **kwargs):
     Returns:
         -
     '''
-    print("PlotCaseRun in TwoDSubduction0: operating")
+    print("PlotCaseRun in FOO: operating")
     # get case parameters
     prm_path = os.path.join(case_path, 'output', 'original.prm')
 
-    # plot visit
-    Visit_Options = VISIT_OPTIONS(case_path)
-    # provide steps to plot and interpret
-    step = kwargs.get('step', None)
-    if type(step) == int:
-        Visit_Options.Interpret(steps=[step])  # only plot a single step
-    else:
-        Visit_Options.Interpret(last_step=3)  # by default, plot the last 3 steps
-    odir = os.path.join(case_path, 'visit_scripts')
-    if not os.path.isdir(odir):
-        os.mkdir(odir)
-    py_script = 'slab.py'
-    ofile = os.path.join(odir, py_script)
-    visit_script = os.path.join(ASPECT_LAB_DIR, 'visit_scripts', 'TwoDSubduction', py_script)
-    visit_script_base = os.path.join(ASPECT_LAB_DIR, 'visit_scripts', 'base.py')
-    Visit_Options.read_contents(visit_script_base, visit_script)  # combine these two scripts
-    Visit_Options.substitute()
-    ofile_path = Visit_Options.save(ofile, relative=True)
-    print("Visualizing using visit")
-    RunScripts(ofile_path)  # run scripts
+    # # plot visit
+    # Visit_Options = VISIT_OPTIONS(case_path)
+    # # provide steps to plot and interpret
+    # step = kwargs.get('step', None)
+    # if type(step) == int:
+    #     Visit_Options.Interpret(steps=[step])  # only plot a single step
+    # else:
+    #     Visit_Options.Interpret(last_step=3)  # by default, plot the last 3 steps
+    # odir = os.path.join(case_path, 'visit_scripts')
+    # if not os.path.isdir(odir):
+    #     os.mkdir(odir)
+    # py_script = 'slab.py'
+    # ofile = os.path.join(odir, py_script)
+    # visit_script = os.path.join(ASPECT_LAB_DIR, 'visit_scripts', 'TwoDSubduction', py_script)
+    # visit_script_base = os.path.join(ASPECT_LAB_DIR, 'visit_scripts', 'base.py')
+    # Visit_Options.read_contents(visit_script_base, visit_script)  # combine these two scripts
+    # Visit_Options.substitute()
+    # ofile_path = Visit_Options.save(ofile, relative=True)
+    # print("Visualizing using visit")
+    # RunScripts(ofile_path)  # run scripts
 
 class PLOTTER(PlotCase.PLOTTER):
     '''
@@ -133,16 +123,12 @@ class PLOTTER(PlotCase.PLOTTER):
         PlotCase.PLOTTER.__init__(self, module, PlotCaseRuns)   
 
 
-def PrScriptToUse(case_path, template_for_chunk, template_for_box):
+def PrScriptToUse(case_path, default):
     '''
     Return model geometry
     Inputs:
     '''
-    Prepare_Result = PREPARE_RESULT_OPTIONS(case_path)
-    if Prepare_Result.get_geometry() == "chunk":
-        pr_script = template_for_chunk
-    elif Prepare_Result.get_geometry() == "box":
-        pr_script = template_for_box
+    pr_script = default
     return pr_script
 
 
@@ -181,9 +167,7 @@ def main():
         pass
     arg = parser.parse_args(_options)
 
-    default_chunk = os.path.join(ASPECT_LAB_DIR, "files", "TwoDSubduction", "figure_step_template.json")
-    default_box = os.path.join(ASPECT_LAB_DIR, "files", "TwoDSubduction", "figure_step_template_box.json")
-
+    default = os.path.join(ASPECT_LAB_DIR, "files", "FOO", "figure_step_template.json")
 
     # commands
     # rearrange the time_range entry
@@ -198,33 +182,21 @@ def main():
         PlotCase.PlotCaseCombinedDir([PlotCase.PlotCaseRun, PlotCaseRun], arg.inputs, time_range=time_range)
         pass
     elif _commend == 'prepare_result_step':
-        pr_script = PrScriptToUse(arg.inputs, default_chunk, default_box)
+        pr_script = PrScriptToUse(arg.inputs, default)
         Plotter = PLOTTER(PREPARE_RESULT_OPTIONS, [PlotCase.PlotCaseRun, PlotCaseRun])
         Plotter.PrepareResultStep(arg.inputs, pr_script, arg.step)
     elif _commend == 'plot_prepare_result_step':
-        pr_script = PrScriptToUse(arg.inputs, default_chunk, default_box)
+        pr_script = PrScriptToUse(arg.inputs, default)
         Plotter = PLOTTER(PREPARE_RESULT_OPTIONS, [PlotCase.PlotCaseRun, PlotCaseRun])
         Plotter.PlotPrepareResultStep(arg.inputs, pr_script, arg.step)
     elif _commend == 'animate_case':
-        pr_script = PrScriptToUse(arg.inputs, default_chunk, default_box)
+        pr_script = PrScriptToUse(arg.inputs, default)
         Plotter = PLOTTER(PREPARE_RESULT_OPTIONS, [PlotCase.PlotCaseRun, PlotCaseRun])
         PlotCase.AnimateCaseResults(Plotter.PlotPrepareResultStep, arg.inputs, pr_script)
     elif _commend == 'animate_case_in_dir':
-        pr_script = PrScriptToUse(arg.inputs, default_chunk, default_box)
+        pr_script = PrScriptToUse(arg.inputs, default)
         Plotter = PLOTTER(PREPARE_RESULT_OPTIONS, [PlotCase.PlotCaseRun, PlotCaseRun])
         PlotCase.AnimateCombinedDir(Plotter.PlotPrepareResultStep, arg.inputs, pr_script)
-    elif _commend == 'morph_case':
-        # slab_morphology, input is the case name
-        vtk_and_slab_morph_case(arg.inputs, rewrite=arg.rewrite)
-    elif _commend == 'morph_case_in_dir':
-        # slab_morphology for cases in directory, input is the case name
-        PlotCase.PlotCaseCombinedDir([vtk_and_slab_morph_case], arg.inputs, rewrite=arg.rewrite)
-    elif _commend == 'plot_morph':
-        # plot slab morphology
-        SlabPlot = SLABPLOT('slab')
-        prm_file = os.path.join(arg.inputs, 'output', 'original.prm')
-        SlabPlot.ReadPrm(prm_file)
-        SlabPlot.PlotMorph(arg.inputs)
     elif (_commend in ['-h', '--help']):
         # example:
         Usage()
