ba69a31470533c41b8fac93e595b3bcf11e0e49e
Haoyuan Li <hylli@ucdavis.edu>
2022-07-13 11:43:55 -0700

    tests/integration/test_PhaseDiagram_Cases.py: add tests for tangential bc on top and bottom, no slip on both sides; box geometry

--- tests/integration/test_PhaseDiagram_Cases.py
+++ tests/integration/test_PhaseDiagram_Cases.py
@@ -82,6 +82,18 @@ def test_bd_lsolver():
     prm_std_path = os.path.join(source_case_dir, 'case_std.prm')
     prm_path = os.path.join(output_dir, 'case.prm')
     assert(filecmp.cmp(prm_path, prm_std_path))
+    # test 3: tangential bc on top and bottom, no slip on both sides; box geometry
+    source_case_dir = os.path.join(source_dir, "test_bd_lsolver_ns_box")
+    json_path = os.path.join(source_case_dir, 'case.json')
+    output_dir = os.path.join(test_dir,'test_PhaseDiagram_bd_lsolver_ns_box')
+    if os.path.isdir(output_dir):
+        rmtree(output_dir)
+    create_case_with_json(json_path, CASE, CASE_OPT)
+    assert(os.path.isdir(output_dir))  # check case generation
+    prm_std_path = os.path.join(source_case_dir, 'case_std.prm')
+    prm_path = os.path.join(output_dir, 'case.prm')
+    assert(filecmp.cmp(prm_path, prm_std_path))
+
 
 
     
