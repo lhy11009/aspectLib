df81ac9163df1564ff59bd86621645e1b1a9e8ad
Haoyuan Li <hylli@ucdavis.edu>
2022-07-25 12:04:35 -0700

    ThDSubduction: update newtonian tests

---
+++ files/ThDSubduction/07252022/job_p-billen.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+#SBATCH -J task
+#SBATCH -N 4
+#SBATCH -n 512
+#SBATCH --threads-per-core=1
+#SBATCH --tasks-per-node=128
+#SBATCH -o task-%j.stdout
+#SBATCH -e task-%j.stderr
+#SBATCH -t 300:00:00
+#SBATCH --partition=p-billen
+#SBATCH --switches=1
+#SBATCH --mem-per-cpu=4G
+
+module unload openmpi/4.0.1
+module load openmpi/4.1.0-mpi-io
+>&2 echo "list of modules:"
+>&2 module list
+>&2 echo "aspect source: ${ASPECT_SOURCE_DIR}"
+
+srun  ${ASPECT_SOURCE_DIR}/build_master_TwoD/aspect case.prm
