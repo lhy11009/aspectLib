55df304e148a009166861c84f05e3d5c0af8902c
Haoyuan Li <hylli@ucdavis.edu>
2023-06-14 11:17:59 -0700

    shilofue/Scripting.py: add scripting

---
+++ shilofue/Scripting.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+r"""(one line description)
+
+This exports: 
+
+  -
+
+This depends on:
+
+  -  
+
+Examples of usage:
+
+  - default usage:
+
+        python -m 
+
+descriptions
+""" 
+import numpy as np
+import sys, os, argparse, re
+# import json
+# import pathlib
+# import subprocess
+import numpy as np
+# from matplotlib import cm
+from matplotlib import pyplot as plt
+
+# directory to the aspect Lab
+ASPECT_LAB_DIR = os.environ['ASPECT_LAB_DIR']
+RESULT_DIR = os.path.join(ASPECT_LAB_DIR, 'results')
+# directory to shilofue
+shilofue_DIR = os.path.join(ASPECT_LAB_DIR, 'shilofue')
+# import utilities in subdirectiory
+sys.path.append(os.path.join(ASPECT_LAB_DIR, 'utilities', "python_scripts"))
+import Utilities
+
+def Usage():
+    print("\
+(One liner description\n\
+\n\
+Examples of usage: \n\
+\n\
+  - default usage: \n\
+\n\
+        python -m \
+        ")
+
+####
+# Utility functions
+####
+
+def ExplicitImport(module, _object, _type):
+    '''
+    explicitly import object from module
+    Inputs:
+        module: name of the module
+        _object: name of the object
+    Returns:
+        contents of the object
+    '''
+    # todo_import
+    assert(_type in ["class", "function"])
+    if _type == "class":
+        prefix = "class"
+    else:
+        prefix = "def"
+    # find the path of the file
+    file_path = ASPECT_LAB_DIR
+    for part in module.split('.'):
+        file_path = os.path.join(file_path, part)
+    file_path += '.py'
+    Utilities.my_assert(os.path.isfile(file_path), FileExistsError, "%s doesn't exist" % file_path)
+    # import the object
+    fin = open(file_path, 'r')
+    line = fin.readline()
+    content_list = []
+    while line != "":
+        if re.match("%s %s" % (prefix, _object), line):
+            content_list.append(line)
+            line = fin.readline()
+            while line != "":
+                if re.match("^(class|def)", line):
+                    # next function is reached, stop
+                    break
+                content_list.append(line)
+                line = fin.readline()
+            break
+        line = fin.readline()
+    # remove the vacant lines at the end
+    for i in range(len(content_list)-1, -1, -1):
+        content = content_list[i]
+        if re.match('(\t| )*\n$', content):
+            content_list.pop(i)
+        else:
+            break
+    # construct the contents
+    contents = ""
+    for content in content_list:
+        contents += content
+    print(contents) # debug
+    return contents
+
+
+def main():
+    '''
+    main function of this module
+    Inputs:
+        sys.arg[1](str):
+            commend
+        sys.arg[2, :](str):
+            options
+    '''
+    _commend = sys.argv[1]
+    # parse options
+    parser = argparse.ArgumentParser(description='Parse parameters')
+    parser.add_argument('-i', '--inputs', type=str,
+                        default='',
+                        help='Some inputs')
+    _options = []
+    try:
+        _options = sys.argv[2: ]
+    except IndexError:
+        pass
+    arg = parser.parse_args(_options)
+
+    # commands
+    if (_commend in ['-h', '--help']):
+        # example:
+        Usage()
+    elif _commend == 'foo':
+        # example:
+        SomeFunction('foo')
+    else:
+        # no such option, give an error message
+        raise ValueError('No commend called %s, please run -h for help messages' % _commend)
+
+# run script
+if __name__ == '__main__':
+    main()
---
+++ tests/integration/fixtures/test_scripting/test_explicit_import_std0.py
@@ -0,0 +1,265 @@
+class STATISTICS_PLOT(LINEARPLOT):
+    '''
+    Class for plotting depth average file.
+    This is an inheritage of the LINEARPLOT class
+
+    Attributes:
+    Args:
+    '''
+    def __init__(self, _name, **kwargs):
+        LINEARPLOT.__init__(self, _name, kwargs)  # call init from base function
+    
+    def GetStep(self, time):
+        '''
+        Inputs:
+            time(double)
+        get step corresponding to a value of model time
+        '''
+        # get data
+        col_t = self.header['Time']['col']
+        col_step = self.header['Time_step_number']['col']
+        times = self.data[:, col_t]
+        steps = self.data[:, col_step]
+
+        # get step
+        idx = np.argmin(abs(times - time))
+        step = int(steps[idx])
+        return step
+    
+    def GetTime(self, step):
+        '''
+        Inputs:
+            step(int)
+        get time to a value of model step
+        '''
+        col_t = self.header['Time']['col']
+        col_step = self.header['Time_step_number']['col']
+        times = self.data[:, col_t]
+        steps = self.data[:, col_step]
+        assert(len(times) == len(steps))
+        time = 0.0
+        found = False
+        for i in range(len(steps)):  # search for step
+            if step == int(steps[i]):
+                time = times[i]
+                found = True
+        Utilities.my_assert(found, ValueError, "step %d is not a valid step" % step)
+        return time
+
+    def GetLastStep(self):
+        '''
+        get step and time of the last time step
+        Return:
+            last step, model time of the last step
+        '''
+        # get data
+        col_t = self.header['Time']['col']
+        col_step = self.header['Time_step_number']['col']
+        times = self.data[:, col_t]
+        steps = self.data[:, col_step]
+        idx = np.argmax(steps)
+        return int(steps[idx]), times[idx]
+
+    def PlotNumberOfCells(self, **kwargs):
+        '''
+        plot the number of cells
+        '''
+        ax = kwargs.get('axis', None)
+        label = kwargs.get('label', None)
+        color = kwargs.get('color', None)
+        if ax == None:
+            raise ValueError("Not implemented")
+        col_t = self.header['Time']['col']
+        unit_t = self.header['Time']['unit']
+        col_step = self.header['Time_step_number']['col']
+        col_noc = self.header['Number_of_mesh_cells']['col']
+        times = self.data[:, col_t]
+        steps = self.data[:, col_step]
+        nocs = self.data[:, col_noc]
+        if self.UnitConvert is not None:
+            to_myr = self.UnitConvert(unit_t, 'myr')
+        else:
+            raise ValueError("a UNITCONVERT class must be given")
+        ax.plot(times * to_myr, nocs, label=label, color=color)
+        ax.set_xlabel('Time (myr)')
+        ax.set_ylabel('Number of mesh cells')
+        pass
+    
+    def PlotNumberOfNonlinearIterations(self, **kwargs):
+        '''
+        plot the number of cells
+        '''
+        ax = kwargs.get('axis', None)
+        label = kwargs.get('label', None)
+        color = kwargs.get('color', None)
+        if ax == None:
+            raise ValueError("Not implemented")
+        col_t = self.header['Time']['col']
+        unit_t = self.header['Time']['unit']
+        col_step = self.header['Time_step_number']['col']
+        col_noni = self.header['Number_of_nonlinear_iterations']['col']
+        times = self.data[:, col_t]
+        steps = self.data[:, col_step]
+        nonis = self.data[:, col_noni]
+        if self.UnitConvert is not None:
+            to_myr = self.UnitConvert(unit_t, 'myr')
+        else:
+            raise ValueError("a UNITCONVERT class must be given")
+        ax.plot(times * to_myr, nonis, '.', label=label, color=color)
+        ax.set_xlabel('Time (myr)')
+        ax.set_ylabel('Number of nonlinear iterations')
+    
+    def PlotNumberOfIterationsStokessolver(self, **kwargs):
+        '''
+        plot the number of cells
+        '''
+        ax = kwargs.get('axis', None)
+        label = kwargs.get('label', None)
+        color = kwargs.get('color', None)
+        if ax == None:
+            raise ValueError("Not implemented")
+        col_t = self.header['Time']['col']
+        unit_t = self.header['Time']['unit']
+        col_step = self.header['Time_step_number']['col']
+        col_iter_stokes = self.header['Iterations_for_Stokes_solver']['col']
+        times = self.data[:, col_t]
+        steps = self.data[:, col_step]
+        iter_stokes = self.data[:, col_iter_stokes]
+        if self.UnitConvert is not None:
+            to_myr = self.UnitConvert(unit_t, 'myr')
+        else:
+            raise ValueError("a UNITCONVERT class must be given")
+        ax.plot(times * to_myr, iter_stokes, '.', label=label, color=color)
+        ax.set_xlabel('Time (myr)')
+        ax.set_ylabel('Number of Iterations for Stokes solver')
+    
+    def PlotDegreeOfFreedom(self, **kwargs):
+        '''
+        plot the number of cells
+        '''
+        ax = kwargs.get('axis', None)
+        label = kwargs.get('label', None)
+        label_all = kwargs.get('label_all', False) # figure out labels
+        if label_all:
+            label_stokes = "(stokes)"
+            label_temperature = "(temperature)"
+            label_composition = "(composition)"
+            if label == None:
+                label_total = "(total)"
+            else:
+                label_total = label + " (total)"
+        else:
+            label_stokes = None
+            label_temperature = None
+            label_composition = None
+            label_total = label
+        color = kwargs.get('color', None)
+        if ax == None:
+            raise ValueError("Not implemented")
+        col_t = self.header['Time']['col']
+        unit_t = self.header['Time']['unit']
+        col_step = self.header['Time_step_number']['col']
+        col_dof_stokes = self.header['Number_of_Stokes_degrees_of_freedom']['col']
+        col_dof_temperature = self.header['Number_of_temperature_degrees_of_freedom']['col']
+        col_dof_composition = self.header['Number_of_degrees_of_freedom_for_all_compositions']['col']
+        times = self.data[:, col_t]
+        steps = self.data[:, col_step]
+        dofs_stokes = self.data[:, col_dof_stokes]
+        dofs_temperature = self.data[:, col_dof_temperature]
+        dofs_composition = self.data[:, col_dof_composition]
+        dofs_total = dofs_stokes + dofs_temperature + dofs_composition
+        if self.UnitConvert is not None:
+            to_myr = self.UnitConvert(unit_t, 'myr')
+        else:
+            raise ValueError("a UNITCONVERT class must be given")
+        ax.plot(times * to_myr, dofs_total, '-', color=color, label=label_total)
+        ax.plot(times * to_myr, dofs_stokes, ':', color=color, label=label_stokes)
+        ax.plot(times * to_myr, dofs_temperature, '--', color=color, label=label_temperature)
+        ax.plot(times * to_myr, dofs_composition, '-.', color=color, label=label_composition)
+        ax.set_xlabel('Time (myr)')
+        ax.set_ylabel('Number of degree of freedoms')
+        pass
+    
+    def PlotTemperature(self, **kwargs):
+        '''
+        plot the number of cells
+        '''
+        ax = kwargs.get('axis', None)
+        label = kwargs.get('label', None)
+        label_all = kwargs.get('label_all', False) # figure out labels
+        if label_all:
+            label_average = "(average temperature)"
+            label_minimum = "(minimum temperature)"
+            label_maximum = "(maximum temperature)"
+            if label == None:
+                label_average = "(average temperature)"
+            else:
+                label_average = label + "(average temperature)"
+        else:
+            label_average = None
+            label_minimum = None
+            label_maximum = None
+            label_average = label
+        color = kwargs.get('color', None)
+        if ax == None:
+            raise ValueError("Not implemented")
+        col_t = self.header['Time']['col']
+        unit_t = self.header['Time']['unit']
+        col_step = self.header['Time_step_number']['col']
+        col_min_T = self.header['Minimal_temperature']['col']
+        col_avg_T = self.header['Average_temperature']['col']
+        col_max_T = self.header['Maximal_temperature']['col']
+        times = self.data[:, col_t]
+        steps = self.data[:, col_step]
+        min_Ts = self.data[:, col_min_T]
+        avg_Ts = self.data[:, col_avg_T]
+        max_Ts = self.data[:, col_max_T]
+        if self.UnitConvert is not None:
+            to_myr = self.UnitConvert(unit_t, 'myr')
+        else:
+            raise ValueError("a UNITCONVERT class must be given")
+        ax.plot(times * to_myr, avg_Ts, '-', color=color, label=label_average)
+        ax.plot(times * to_myr, min_Ts, '-.', color=color, label=label_minimum)
+        ax.plot(times * to_myr, max_Ts, '--', color=color, label=label_maximum)
+        ax.set_xlabel('Time (myr)')
+        ax.set_ylabel('Temperature (K)')
+        pass
+    
+    def PlotVelocity(self, **kwargs):
+        '''
+        plot the velocity outputs
+        '''
+        ax = kwargs.get('axis', None)
+        label = kwargs.get('label', None)
+        label_all = kwargs.get('label_all', False) # figure out labels
+        if label_all:
+            label_maximum = "(maximum velocity)"
+            if label == None:
+                label_rms = "(rms velocity)"
+            else:
+                label_rms = label + "(rms velocity)"
+        else:
+            label_maximum = None
+            label_rms = label
+        color = kwargs.get('color', None)
+        if ax == None:
+            raise ValueError("Not implemented")
+        col_t = self.header['Time']['col']
+        unit_t = self.header['Time']['unit']
+        col_step = self.header['Time_step_number']['col']
+        col_rms_V = self.header['RMS_velocity']['col']
+        unit_V = self.header['RMS_velocity']['unit']
+        col_max_V = self.header['Max._velocity']['col']
+        times = self.data[:, col_t]
+        steps = self.data[:, col_step]
+        rms_Vs = self.data[:, col_rms_V]
+        max_Vs = self.data[:, col_max_V]
+        if self.UnitConvert is not None:
+            to_myr = self.UnitConvert(unit_t, 'myr')
+        else:
+            raise ValueError("a UNITCONVERT class must be given")
+        ax.plot(times * to_myr, rms_Vs, '-', color=color, label=label_rms)
+        ax.plot(times * to_myr, max_Vs, '--', color=color, label=label_maximum)
+        ax.set_xlabel('Time (myr)')
+        ax.set_ylabel('Velocity (%s)' % unit_V)
+        pass
