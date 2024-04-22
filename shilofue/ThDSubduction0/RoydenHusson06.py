23713264d3734d5024d4c4437d41a9cb8e70ebb8
Haoyuan Li <hylli@ucdavis.edu>
2024-04-22 12:38:42 -0700

    follow

--- shilofue/ThDSubduction0/RoydenHusson06.py
+++ shilofue/ThDSubduction0/RoydenHusson06.py
@@ -19,6 +19,7 @@ descriptions
 """ 
 import numpy as np
 import sys, os, argparse
+import math
 # import json, re
 # import pathlib
 # import subprocess
@@ -416,6 +417,141 @@ def symmetric_grid(xmin, xmax, nx, ymin, ymax, ny):
     ys = np.linspace(-ymax, ymax, 3*ny+1)
     return xs, ys
 
+class SLAB():
+    '''
+    class for slab profile
+    Attributes:
+        Vs: velocity along s
+        Vn: velocity along n
+        Taus: shear stress
+        SigNs: normal stress
+        pSigN_pSs: normal stress derivatives
+        P0: viscous pressure from large scale flow around the slab
+    '''
+    def __init__(self, lbd, mu):
+        self.n = 100
+        self.lbd = lbd
+        # initiate the profile of x and y
+        self.xs = np.zeros(self.n)
+        self.zs = np.linspace(0, self.lbd, self.n)
+        self.r0s = np.zeros(self.n)
+        self.theta0s = np.zeros(self.n)
+        self.Vs = 0.0
+        self.Vns = np.zeros(self.n)
+        self.pSigN_pSs = np.zeros(self.n)
+        self.Taus = np.zeros(self.n)
+        self.mu = mu
+        self.SigNs = np.zeros(self.n)
+        self.P0 = 0.0
+
+    def LocalProfile(self, i):
+        '''
+        '''
+        assert(i < self.n)
+        # value of the interpolation points
+        x0 = 0.0
+        y0 = 0.0
+        z0 = 0.0
+        z1 = 0.0
+        if i == 0:
+            x0 = self.xs[0]
+            x1 = self.xs[1]
+            z0 = self.zs[0]
+            z1 = self.zs[1]
+        elif i == self.n-1:
+            x0 = self.xs[self.n-2]
+            x1 = self.xs[self.n-1]
+            z0 = self.zs[self.n-2]
+            z1 = self.zs[self.n-1]
+        else:
+            x0 = self.xs[i-1]
+            x1 = self.xs[i+1]
+            z0 = self.zs[i-1]
+            z1 = self.zs[i+1]
+        theta0 = math.atan2(x1 -x0, z1-z0)
+        r0 = (self.lbd - self.zs[i]) / math.asin(theta0)
+        return r0, theta0
+
+    def UpdateProfile(self):
+        '''
+        Update the slab profile
+        '''
+        for i in range(self.n):
+            r0, theta0 = self.LocalProfile(i)
+            self.r0s[i] = r0
+            self.theta0s[i] = theta0
+    
+    def PlotProfile(self, **kwargs):
+        '''
+        plot the slab profile
+        '''
+        ax = kwargs.get('ax', None)
+        ax.plot(self.xs/1e3, self.zs/1e3)
+        # plot properties
+        ax.set_xlabel("X (km)")
+        ax.set_xlim([0.0, self.lbd*2.0/1e3])
+        ax.set_ylabel("Z (km)")
+        ax.set_ylim([0.0, self.lbd/1e3])
+        ax.invert_xaxis()
+        ax.invert_yaxis()
+        ax.grid()
+
+    def InitiateProfile(self, S, theta):
+        '''
+        assign an initial profile
+        '''
+        for i in range(self.n):
+            self.xs[i] = S * i / (self.n - 1.0) * math.cos(theta)
+            self.zs[i] = S * i/ (self.n - 1.0) * math.sin(theta)
+        self.UpdateProfile()
+    
+    def UpdateVs(self, Vs):
+        '''
+        update Vs on the slab
+        '''
+        self.Vs = Vs
+
+    def calculate_SigN_Tau(self):
+        '''
+        eq 5, 6
+        '''
+        # sum over Vn
+        sum_Vns = np.zeros(self.n)
+        # derive normal and shear stress
+        for i in range(self.n):
+            r0 = self.r0s[i]
+            theta0 = self.theta0s[i]
+            Vn = self.Vns[i]
+            Vs = self.Vs
+            a = -12.0 * self.mu / (r0**3.0 * theta0**3.0) * sum_Vns[i]
+            b = 6 * self.mu * (Vn + Vs) / r0**2.0 / theta0**2.0
+            self.pSigN_pSs[i] = a + b  
+            self.Taus[i] = -r0 * theta0 * self.pSigN_pSs[i] + self.mu * (Vn - Vs) / r0 / theta0
+        self.SigNs[0] = self.P0
+        for i in range(1, self.n):
+            dz = self.zs[i] - self.zs[i-1]
+            self.SigNs[i] = self.SigNs[i-1] + (self.pSigN_pSs[i-1] + self.pSigN_pSs[i]) * dz / 2.0
+    
+    def calculate_P0(self):
+        '''
+        eq 4
+        '''
+        pass
+
+    def PlotStresses(self, **kwargs):
+        '''
+        Plot the stress profile
+        '''
+        ax = kwargs.get('ax', None)
+        ax.plot(self.SigNs / 1e6, self.zs/1e3, label="normal")
+        ax.plot(self.Taus / 1e6, self.zs/1e3, label="shear")
+        # plot properties
+        ax.set_xlabel("Stress (Mpa)")
+        ax.set_ylabel("Z (km)")
+        ax.set_ylim([0.0, self.lbd/1e3])
+        ax.invert_yaxis()
+        ax.legend()
+        ax.grid()
 
 
 def main():
