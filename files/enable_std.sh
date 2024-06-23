763ef9a76b0c38dca25af616ce97078cc617626e
Haoyuan Li <hylli@ucdavis.edu>
2024-06-22 21:36:24 -0700

    minor changes

--- files/enable_std.sh
+++ files/enable_std.sh
@@ -9,3 +9,6 @@ export EntropySub_DIR="/home/lochy/ASPECT_PROJECT/EntropySub_DIR"
 alias Lib_TwoDSubduction0_run_docker="xhost + && docker run --user root --rm -ti --net=host --ipc=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --env="QT_X11_NO_MITSHM=1" -it visitdev:3.1.3-ubuntu16-work"
 # run the docker tester
 alias Lib_EntropySub_aspect_run_docker="docker run -v $ASPECT_SOURCE_DIR:/home/dealii/aspect -v $ASPECT_LAB_DIR/bash_scripts/build_aspect_docker.sh:/opt/build_aspect_docker.sh -v $WORLD_BUILDER_SOURCE_DIR:/home/worldbuilder --name=aspect-tester --rm -it geodynamics/aspect-tester:focal-dealii-9.4-v3"
+##### local rsync commands #####
+# sync case restart files between two local folders
+alias rsync_restart_local="rsync -avu --progress --exclude=*restart*.old --exclude=*solution* ${SCRATCH}/ThDSubduction/* ${ThDSubduction_DIR}/"
