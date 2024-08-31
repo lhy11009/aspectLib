e5585dbf515266a4b4aa19625a8ae50c52956389
Haoyuan Li <hylli@ucdavis.edu>
2024-08-31 11:07:39 -0700

    small change

--- env/enable_frontera.sh
+++ env/enable_frontera.sh
@@ -7,6 +7,7 @@ export ThDSubduction_DIR="/scratch1/06806/hyli/ThDSubduction"
 alias all_case_info="Lib_parse_case all_case_info"
 alias all_case_info_in_dir="Lib_parse_case case_info_in_dir ."
 alias rsync_restart_local="rsync -avu --progress --exclude=*restart*.old --exclude=*solution* ${SCRATCH}/ThDSubduction/* /work2/06806/hyli/frontera/ThDSubduction/"
+alias rm_restart_local="rm \`find /work2/06806/hyli/frontera/ThDSubduction/ -name "*restart*"\`"
 # si, sq: sinfo and squeue
 alias si="sinfo -o \"%20P %5D %14F %8z %10m %10d %11l %16f %N\""
 alias sq="squeue -u hyli -o \"%8i %12j %4t %10u %20q %20a %10g %20P %10Q %5D %11l %11L %R\""
